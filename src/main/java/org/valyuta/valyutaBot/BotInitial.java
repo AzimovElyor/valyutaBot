586d24620d5241361b51f94678a5d3e3e3369db7
AzimovELyor <elyorazimov098@gmail.com>
2023-11-20 12:25:19 +0500

    Manifest added

--- src/main/java/org/valyuta/valyutaBot/BotInitial.java
+++ src/main/java/org/valyuta/valyutaBot/BotInitial.java
@@ -5,12 +5,6 @@ import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
-import java.time.LocalDate;
-import java.time.LocalTime;
-import java.time.ZoneId;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-
 
 public class   BotInitial {
     public static void main(String[] args) throws TelegramApiException {
